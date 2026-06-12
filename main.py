bb4ed02528ee6e7c6d5615079fd9ec328e5bb7d2
and_cherr <andre.cherkashini@gmail.com>
2026-06-12 15:54:45 +0400

    add rate limiting

--- main.py
+++ main.py
@@ -2,10 +2,19 @@ from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 from database import SessionLocal, engine, Base
 from models import Item
+from slowapi import Limiter
+from slowapi.util import get_remote_address
 
 Base.metadata.create_all(bind=engine)
 
+limiter = Limiter(key_func=get_remote_address)
 app = FastAPI()
+app.state.limiter = limiter
+
+from slowapi.errors import RateLimitExceeded
+from slowapi import _rate_limit_exceeded_handler
+
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 def get_db():
     db = SessionLocal()
@@ -16,6 +25,7 @@ def get_db():
 
 
 @app.post("/items")
+@limiter.limit("10/minute")
 def create_item(name: str, description: str, price: float = None, db: Session = Depends(get_db)):
     item = Item(name=name, description=description, price=price)
     db.add(item)
@@ -25,11 +35,13 @@ def create_item(name: str, description: str, price: float = None, db: Session = 
 
 
 @app.get("/item/{item_id}")
+@limiter.limit("10/minute")
 def get_item(item_id: int, db: Session = Depends(get_db)):
     item = db.query(Item).filter(Item.id == item_id).first()
     return item
 
 
 @app.get("/items")
+@limiter.limit("10/minute")
 def get_items(db: Session = Depends(get_db)):
     return db.query(Item).all()
